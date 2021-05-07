ad9b764a8a690dc6189d14b44572ef688c20b810
ivushka24 <ivushka24@tut.by>
2021-05-07 23:32:42 +0300

    image

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 path('post/new/', views.post_new, name='post_new'),
 path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-path('dance/', views.dance,),
+path('dance/', views.dance),
+path('static/blog/a2ec8c82a0b3d1c2393ebe4f919e9d43.gif', views.dance),
 ]
